c918eaf7400e5b9d45dc819bdbd00d597170e459
TagRomEdu <qwertystyles@gmail.com>
2023-09-11 13:14:17 +0500

    reworked class

--- src/api_worker.py
+++ src/api_worker.py
@@ -4,9 +4,9 @@ import requests
 
 class APIWorker(ABC):
     @abstractmethod
-    def get_vacancies(self, text):
+    def get_company_list(self, text):
         """
-        method returns list of vacancies
+        method returns list of 10 companies
         """
         pass
 
@@ -14,6 +14,9 @@ class APIWorker(ABC):
 class HeadHunterAPI(APIWorker):
     HH_VACANCIES = 'https://api.hh.ru/vacancies'
 
-    def get_vacancies(self, text: str) -> dict:
-        request = requests.get(self.HH_VACANCIES + f"?text={text.lower()}").json()
-        return request
+    def get_company_list(self, text: str) -> list:
+        vac_data = requests.get(self.HH_VACANCIES + f"?text={text.lower()}").json()
+        primary_info = vac_data["items"]
+        companies_list = [vacancy["employer"]["name"] for vacancy in primary_info]
+
+        return list(set(companies_list))[:10]
