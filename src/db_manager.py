b5198f71b09d2c6d88a781e7ee1e376172ced22a
TagRomEdu <qwertystyles@gmail.com>
2023-09-11 12:44:38 +0500

    add method get_companies_and_vacancies_count

--- src/db_manager.py
+++ src/db_manager.py
@@ -3,10 +3,20 @@ import psycopg2
 
 
 class DBManager:
-    HH_EMPLOYEES = 'https://api.hh.ru/employers'
+    HH_EMPLOYERS = 'https://api.hh.ru/employers'
 
-    def get_companies_and_vacancies_count(self):
-        pass
+    def get_companies_and_vacancies_count(self, company_lst: list) -> dict:
+        """
+        Returns dict of companies and vacancies' count
+        """
+        company_dict = {}
+        for company in company_lst:
+            company_data = requests.get(self.HH_EMPLOYERS + f"?text={company.lower()}").json()
+            primary_info = company_data["items"][0]
+
+            company_dict[primary_info["name"]] = len(requests.get(primary_info["vacancies_url"]).json()["items"])
+
+        return company_dict
 
     def get_all_vacancies(self):
         pass
@@ -19,3 +29,7 @@ class DBManager:
 
     def get_vacancies_with_keyword(self):
         pass
+
+
+
+
