b43cc3392e0da220af686cd4ec305b7836a7e0e2
TagRomEdu <qwertystyles@gmail.com>
2023-09-11 10:40:57 +0500

    formed a class and a method for requesting information via the API hh.ru

--- src/api_worker.py
+++ src/api_worker.py
@@ -1,15 +1,8 @@
 from abc import ABC, abstractmethod
-import os
 import requests
 
 
 class APIWorker(ABC):
-    API_SJ = os.getenv('API_SuperJob')
-    HH_VACANCIES = 'https://api.hh.ru/vacancies'
-    SJ_VACANCIES = 'https://api.superjob.ru/2.0/vacancies'
-
-    headers_sj = {'X-Api-App-Id': API_SJ}
-
     @abstractmethod
     def get_vacancies(self, text):
         """
@@ -19,15 +12,8 @@ class APIWorker(ABC):
 
 
 class HeadHunterAPI(APIWorker):
+    HH_VACANCIES = 'https://api.hh.ru/vacancies'
 
     def get_vacancies(self, text: str) -> dict:
         request = requests.get(self.HH_VACANCIES + f"?text={text.lower()}").json()
-        return request
-
-
-class SuperJobAPI(APIWorker):
-
-    def get_vacancies(self, text: str) -> dict:
-        req_str = self.SJ_VACANCIES + f"?keyword={text.lower()}"
-        request = requests.get(req_str, headers=self.headers_sj).json()
         return request
