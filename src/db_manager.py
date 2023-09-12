c215a73de32429f98069e7597421a4e2f32157c2
TagRomEdu <qwertystyles@gmail.com>
2023-09-12 09:22:52 +0500

    added method get_all_vacancies; rework method get_companies_and_vacancies_count because of adding db_worker (staticmethod)

--- src/db_manager.py
+++ src/db_manager.py
@@ -6,21 +6,31 @@ class DBManager:
         self.db_name = db_name
         self.params = params
 
-    def get_companies_and_vacancies_count(self) -> list[tuple]:
-        """
-        Returns dict of companies and vacancies' count
-        """
-
-        conn = psycopg2.connect(dbname=self.db_name, **self.params)
+    @staticmethod
+    def db_worker(sql_request: str, db_name, params) -> list:
+        conn = psycopg2.connect(dbname=db_name, **params)
         with conn.cursor() as cur:
-            cur.execute("SELECT company_name, open_vacancies FROM employers")
+            cur.execute(sql_request)
             rows = cur.fetchall()
         conn.commit()
         conn.close()
         return rows
 
+    def get_companies_and_vacancies_count(self) -> list[tuple]:
+        """
+        Returns list of companies and vacancies' count
+        """
+        return self.db_worker("SELECT company_name, open_vacancies FROM employers", self.db_name, self.params)
+
     def get_all_vacancies(self):
-        pass
+        """
+        Returns list of all vacancies in db
+        """
+        return self.db_worker("""
+            SELECT employers.company_name, name, CONCAT(salary, ' ', currency) AS salary, vacancy_url FROM vacancies
+            INNER JOIN  employers USING(company_id)
+            """, self.db_name, self.params)
+
 
     def get_avg_salary(self):
         pass
